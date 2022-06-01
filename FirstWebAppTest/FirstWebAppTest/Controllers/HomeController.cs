0571b78723f10ffc9c4f54711d98bc862969192a
farruxaliyev299 <100687365+farruxaliyev299@users.noreply.github.com>
2022-06-01 11:32:05 +0400

    Add files via upload

---
+++ FirstWebAppTest/FirstWebAppTest/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstWebAppTest.Models;
+using System.Collections.Generic;
+
+namespace FirstWebAppTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View(GetStudent(id));
+        }
+
+        public Student GetStudent(int? id)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){Id=1,FullName="Ferrux Eliyev"},
+                new Student(){Id=2,FullName="Samir Eliyev"},
+                new Student(){Id=3,FullName="Emil Haciyev"}
+            };
+
+            foreach (var student in students)
+            {
+                if (student.Id == id)
+                {
+                    return student;
+                }
+            }
+            return null;
+        }
+
+        public string Name()
+        {
+            return "Ferrux";
+        }
+    }
+}
---
+++ FirstWebAppTest/FirstWebAppTest/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstWebAppTest.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
